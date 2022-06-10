a86e88f55b3fbf244d15dc00e8127cd6bce22f27
suyangzuo <suyangzuo@163.com>
2022-06-10 16:13:50 +0800

    更换了照片，输入过程改为纯英文。

--- 密码生成器控制台/Program.cs
+++ 密码生成器控制台/Program.cs
@@ -1,24 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using static 密码生成器控制台.Password;
-using Spectre.Console;
-
-string copyRight = "开发名单：苏扬\n开发平台：.NET 6.0\n开发时间：2022年6月2日";
-string hint = "强密码提示：大小写字母+数字+字符，建议密码长度：8-20";
-Console.WriteLine($"{copyRight}\n{hint}\n");
 
 while (true)
 {
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.Write("输入密码长度：");
+    Console.Write("Input password length: ");
     Console.ForegroundColor = ConsoleColor.Gray;
     int.TryParse(Console.ReadLine() ?? string.Empty, out int passwordLength);
 
     Console.ForegroundColor = ConsoleColor.Cyan;
-    Console.WriteLine("选择密码类型：");
+    Console.WriteLine("Choose password type: ");
     Console.ForegroundColor = ConsoleColor.Gray;
-    Console.Write("1. 仅字母\n2. 字母 + 数字\n3. 字母 + 符号\n4. 字母 + 数字 + 符号\n");
+    Console.Write("1. Letters\n2. Letters + Digits\n3. Letters + Symbols\n4. Letters + Digits + Symbols\n");
     Console.ForegroundColor = ConsoleColor.Red;
-    Console.Write("输入序号并回车：");
+    Console.Write("Input number and press enter: ");
     Console.ForegroundColor = ConsoleColor.Gray;
     int.TryParse(Console.ReadLine() ?? string.Empty, out int pwdTypeSelect);
 
