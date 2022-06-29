f5f0315ab886c65cad57b6bccdbb2f526ce29fbc
suyangzuo <suyangzuo@163.com>
2022-06-29 21:06:48 +0800

    策略模式初次提交

--- 密码生成器控制台/PasswordTools/Password.cs
+++ 密码生成器控制台/PasswordTools/Password.cs
@@ -1,6 +1,6 @@
 namespace 密码生成器控制台.PasswordTools
 {
-    public class Password
+    public static class Password
     {
         [Flags]
         public enum PwdType
@@ -14,6 +14,27 @@ namespace 密码生成器控制台.PasswordTools
             LettersAndSymbols = Letters | Symbols,
             All = Letters | Digits | Symbols
         }
+
+        public static char[] PwdCharPool = new Func<char[]>(() =>
+        {
+            var r = new Random(Guid.NewGuid().GetHashCode());
+            var pwdCharPool = new char[94];
+
+            for (int i = 0; i < pwdCharPool.Length; i++)
+            {
+                pwdCharPool[i] = i switch
+                {
+                    < 10 => (char)(i + 48), //ASCII：48-57，数字
+                    < 36 => (char)(i + 55), //ASCII：65-90，大写字母
+                    < 62 => (char)(i + 61), //ASCII：97-122，小写字母
+                    < 77 => (char)(i - 29), //ASCII：33-47，共15个字符，数字之前
+                    < 84 => (char)(i - 19), //ASCII：58-64，共7个字符，大写字母之前
+                    < 90 => (char)(i + 7),  //ASCII：91-96，共6个字符，小写字母之前
+                    _ => (char)(i + 33)     //ASCII：123-126，共4个字符，小写字母之后
+                };
+            }
+            return pwdCharPool;
+        })();
 
         public static string GetPassword(int length, PwdType pwdType)
         {
